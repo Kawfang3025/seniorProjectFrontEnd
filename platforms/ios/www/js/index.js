0c8520439402146e4e49589d13b35a42ae59ffcc
Kawfang3025 <5930213025@email.psu.ac.th>
2019-11-15 03:11:54 +0700

    update

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -1,86 +1,55 @@
-let app = {
-    init: function () {
-      document.addEventListener("deviceready", app.ready);
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
-    ready: function () {
-      app.addListeners();
-    },
-    addListeners: function () {
-      document.querySelector("#add-btn").addEventListener("click", app.addNote);
-      cordova.plugins.notification.local.on("click", function (notification) {
-        navigator.notification.alert("clicked: " + notification.id);
-        //user has clicked on the popped up notification
-        console.log(notification.data);
-      });
-      cordova.plugins.notification.local.on("trigger", function (notification) {
-        //added to the notification center on the date to trigger it.
-        navigator.notification.alert("triggered: " + notification.id);
-      });
-    },
-    addNote: function (ev) {
-      let props = cordova.plugins.notification.local.getDefaults();
-      //console.log(props);
-      /**
-       * Notification Object Properties - use it as a reference later on
-       * id
-       * text
-       * title
-       * every
-       * at
-       * data
-       * sound
-       * badge
-       */
-      let inOneMin = new Date();
-      inOneMin.setMinutes(inOneMin.getMinutes() + 1);
-      let id = new Date().getMilliseconds();
-  
-      let noteOptions = {
-        id: id,
-        title: "This is the Title",
-        text: "Don't forget to do that thing.",
-        at: inOneMin,
-        badge: 1,
-        data: {
-          prop: "prop value",
-          num: 42
-        }
-      };
-  
-      /**
-       * if(props.icon){
-        noteOptions.icon = './img/calendar-md-@2x.png'
-      }
-      if(props.led){
-        noteOptions.led = '#33CC00'
-      }
-      if(props.actions){
-        noteOptions.actions = [{ id: "yes", title: "Do it" }, { id: "no", title: "Nah" }]
-      }
-      **/
-      cordova.plugins.notification.local.schedule(noteOptions, function(note){
-        //this is the callback function for the schedule method
-        //this runs AFTER the notification has been scheduled.
-      });
-  
-      navigator.notification.alert("Added notification id " + id);
-  
-      cordova.plugins.notification.local.cancel(id, function () {
-        // will get rid of notification id 1 if it has NOT been triggered or added to the notification center
-        // cancelAll() will get rid of all of them
-      });
-      cordova.plugins.notification.local.clear(id, function () {
-        // will dismiss a notification that has been triggered or added to notification center
-      });
-      cordova.plugins.notification.local.isPresent(id, function (present) {
-        // navigator.notification.alert(present ? "present" : "not found");
-        // can also call isTriggered() or isScheduled()
-        // getAllIds(), getScheduledIds() and getTriggeredIds() will give you an array of ids
-        // get(), getAll(), getScheduled() and getTriggered() will get the notification based on an id
-      });
-  
-    }
-  };
-  app.init();
 
-  
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+
+    
+};
+
+app.initialize();
+
+function logout(){
+    console.log('logout!');
+    
+    localStorage.clear('email');
+    window.location.href = 'index.html'
+}
